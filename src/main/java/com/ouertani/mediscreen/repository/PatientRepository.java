7e13e622d080500dc2fbe5045bf05f7a1b352ef1
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-11-26 10:06:52 +0100

    Angular Patients list OK

--- src/main/java/com/ouertani/mediscreen/repository/PatientRepository.java
+++ src/main/java/com/ouertani/mediscreen/repository/PatientRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Page<Patient> findByLastNameLikeIgnoreCase(@Nullable String lastName, Pageable pageable);
